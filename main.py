88bc6e0d977243216c4f69e41497652b8413d79e
bethelchoto <74825621+bethelchoto@users.noreply.github.com>
2023-04-04 23:54:35 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import uuid
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from flask import Flask, request, jsonify
 
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
